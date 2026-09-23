23a9fd3a7ea92084a14e1032a755b49d1eab3b67
Arpit_Singh <asr827013@gmail.com>
2026-09-23 11:12:28 +0530

    revoked naming of file extension

--- migrated_frontend/src/pages/data/projectfile.js
+++ migrated_frontend/src/pages/data/projectfile.js
@@ -547,7 +547,7 @@ import anandiView from "../../assets/project-imgs/anandiviewmain.jpg";
 import anandiVilla from "../../assets/project-imgs/anandivillamain.jpg";
 import anandiVilla2 from "../../assets/project-imgs/anandivilla2main.jpg";
 import santiBhawan from "../../assets/project-imgs/santibhawanmain.jpg";
-import anandiEnclave2  from "../../assets/project-imgs/anandienclavemain2.JPEG";
+import anandiEnclave2  from "../../assets/project-imgs/anandienclavemain2.jpeg";
 
 export {
   anandiApartment,
